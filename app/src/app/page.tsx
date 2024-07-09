624b5253eb152e65f35b39cd5ff5c88e5340443d
Fabrice Payet <hello@fabricepayet.fr>
2024-07-09 12:44:19 +0400

    Init nextjs, tailwind and shadcn

---
+++ app/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Hero from "@/components/Hero";
+
+const Home = () => {
+  return (
+    <main className="flex flex-col items-center justify-between bg-black h-screen w-screen">
+      <Hero />
+    </main>
+  )
+}
+
+export default Home;
